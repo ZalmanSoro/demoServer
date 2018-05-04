878e3bc3481b4b99f80f04144ef5f8a4f985c472
zalman sorotzkin <zsorotzk@g.jct.ac.il>
2018-05-04 10:33:21 +0300

    cleaning the app

--- server.js
+++ server.js
@@ -7,11 +7,9 @@ app.use((req,res,next)=>{
     console.log(req.method + req.url);
     next();
 });
-app.use((req,res,next)=>{
-    res.sendFile(__dirname + '/views/loading.html')
-    });
 
-app.use(express.static(__dirname + '/public'));
+
+//app.use(express.static(__dirname + '/public'));
 
 
 
